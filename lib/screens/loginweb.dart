145d3dce93479e6d07f0c09803e192e14dcc82f3
Alberto Martinez Lasheras <72318269+lasheralberto@users.noreply.github.com>
2024-08-23 17:40:17 +0200

    Update loginweb.dart

--- lib/screens/loginweb.dart
+++ lib/screens/loginweb.dart
@@ -93,23 +93,23 @@ colors: [
                   child: AnimatedTextKit(
                     animatedTexts: [
                       TypewriterAnimatedText(
-                          'La inteligencia artificial impulsa tu negocio hacia el futuro', speed: Duration(milliseconds: 100)),
+                          'La inteligencia artificial impulsa tu negocio hacia el futuro', speed: Duration(milliseconds: 50)),
                           
-                     TypewriterAnimatedText('El 75% de los líderes empresariales creen que la IA es esencial para su estrategia de futuro', speed: Duration(milliseconds: 100)),
+                     TypewriterAnimatedText('El 75% de los líderes empresariales creen que la IA es esencial para su estrategia de futuro', speed: Duration(milliseconds: 50)),
                       TypewriterAnimatedText(
-                          'Optimiza procesos, maximiza resultados', speed: Duration(milliseconds:100)),
+                          'Optimiza procesos, maximiza resultados', speed: Duration(milliseconds:50)),
                       TypewriterAnimatedText(
-                          'La eficiencia es la clave del éxito en la era digital', speed: Duration(milliseconds: 100)),
-                     TypewriterAnimatedText('Las empresas que integran IA incrementan un 35% su eficiencia operativa', speed: Duration(milliseconds: 100)),
+                          'La eficiencia es la clave del éxito en la era digital', speed: Duration(milliseconds: 50)),
+                     TypewriterAnimatedText('Las empresas que integran IA incrementan un 35% su eficiencia operativa', speed: Duration(milliseconds: 50)),
                       TypewriterAnimatedText(
-                          'Haz más con menos, gracias a la automatización', speed: Duration(milliseconds: 100)),
+                          'Haz más con menos, gracias a la automatización', speed: Duration(milliseconds: 50)),
                       TypewriterAnimatedText(
-                          'La innovación tecnológica impulsa tu crecimiento', speed: Duration(milliseconds: 100)),
+                          'La innovación tecnológica impulsa tu crecimiento', speed: Duration(milliseconds: 50)),
                       TypewriterAnimatedText(
-                          'Acelera el progreso de tu empresa con IA', speed: Duration(milliseconds: 100)),
-                     TypewriterAnimatedText('La automatización de tareas rutinarias incrementa la creatividad del equipo en un 20%', speed: Duration(milliseconds: 100)),
+                          'Acelera el progreso de tu empresa con IA', speed: Duration(milliseconds: 50)),
+                     TypewriterAnimatedText('La automatización de tareas rutinarias incrementa la creatividad del equipo en un 20%', speed: Duration(milliseconds: 50)),
                       TypewriterAnimatedText(
-                          'El futuro es automatizado, y tu empresa puede estar a la vanguardia', speed: Duration(milliseconds: 100)),
+                          'El futuro es automatizado, y tu empresa puede estar a la vanguardia', speed: Duration(milliseconds: 50)),
                     ],
                     onTap: () {
                       print("Tap Event");
